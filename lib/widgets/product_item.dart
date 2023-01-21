ff185a1ebebdff26267355b7bb033e5577c3be68
Rohit_Dwivedi <rohitdwivedi225@gmail.com>
2023-01-21 17:13:06 +0530

    Creating a shopping app. So far added products overview page and showing a few dummy products. After tapping on the image -> showing a product details page using MaterialPageRoute. In next commit going to use named routes instead.

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({required this.id, required this.title, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (ctx) {
+                return ProductDetailScreen(title);
+              }),
+            );
+          },
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: const Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
