7210a3e0070526f45fe42d6677d503253531c4bd
Rohit_Dwivedi <rohitdwivedi225@gmail.com>
2023-01-21 21:51:39 +0530

    using provider to access the product in product_detail_screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = 'product-details-page';
@@ -7,8 +9,10 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var productId = ModalRoute.of(context)?.settings.arguments as String;
+    final product = Provider.of<Products>(context,listen: false).items.firstWhere((product) => product.id == productId);
+    
     return Scaffold(
-      appBar: AppBar(title: Text(productId)),
+      appBar: AppBar(title: Text(product.title)),
     );
   }
 }
