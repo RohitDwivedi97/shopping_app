c5e9b262fda6472d8176efc5f921b2d6a5cb62a7
Rohit_Dwivedi <rohitdwivedi225@gmail.com>
2023-02-11 21:59:54 +0530

    using then and catchError methods on the Future objects.

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -103,11 +103,29 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) => {
+          .catchError(
+        (_) {
+          return showDialog<void>(
+            context: context,
+            builder: (ctx) => AlertDialog(
+              title: const Text('An error occurred.'),
+              content: const Text('Something went wrong.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                  child: const Text('Okay'),
+                ),
+              ],
+            ),
+          );
+        },
+      ).then((error) => {
                 setState(() {
                   _isLoading = false;
                 }),
-                Navigator.of(context).pop()
+                Navigator.of(context).pop(),
               });
     }
   }
